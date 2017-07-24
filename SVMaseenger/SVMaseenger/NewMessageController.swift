b911833c12c17c808eeee2f31baa2ff95143bce8
Vergirey <sergey_v74@icloud.com>
2017-07-25 02:00:34 +0300

    end Ep4 next "How to Upload Images to Firebase Storage" (Ep5)

--- SVMaseenger/SVMaseenger/NewMessageController.swift
+++ SVMaseenger/SVMaseenger/NewMessageController.swift
@@ -20,6 +20,8 @@ class NewMessageController: UITableViewController {
         
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(handleCancel))
         
+        tableView.register(UserCell.self, forCellReuseIdentifier: cellId)
+        
         fetchUser()
     }
     
@@ -29,14 +31,17 @@ class NewMessageController: UITableViewController {
             if let dictionary = snapshot.value as? [String: AnyObject] {
                 let user = User()
                 
-                //if you use this setter, your app will crash, if your class properties dont's exactly match up qwith tge firebase dictionary keys
+//                if you use this setter, your app will crash, if your class properties dont's exactly match up qwith tge firebase dictionary keys
                 user.setValuesForKeys(dictionary)
+                self.users.append(user)
+                
+//                this will crash because of background thread, so lets use dispetch_async to fix
+                DispatchQueue.main.async(execute: {
+                    self.tableView.reloadData()
+                })
                 
 //                user.name = dictionary["name"]
-                
-                print(user.name!, user.email!)
             }
-            //print(snapshot)
         }, withCancel: nil)
     }
     
@@ -45,18 +50,29 @@ class NewMessageController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return users.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        // let use a hack for now, we actually need to dequeue our cells for memory efficiency
-        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellId)
+//        let use a hack for now, we actually need to dequeue our cells for memory efficiency
+//        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellId)
         
-        cell.textLabel?.text = "Dummy TEXT LALALA"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        let user = users[indexPath.row]
+        cell.textLabel?.text = user.name
+        cell.detailTextLabel?.text = user.email
         
         return cell
     }
+}
 
-   
+class UserCell: UITableViewCell {
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
