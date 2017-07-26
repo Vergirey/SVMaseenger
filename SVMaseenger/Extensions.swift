f451e91a0f879068c2e6634460f076d58439700f
Vergirey <sergey_v74@icloud.com>
2017-07-26 20:54:40 +0300

    end ep 6: "how to load images from Firebase Storage" next ep 7: "Lets Fix some Bugs and Use JPEG Image Compression"

---
+++ SVMaseenger/Extensions.swift
@@ -0,0 +1,43 @@
+//
+//  Extensions.swift
+//  SVMaseenger
+//
+//  Created by Julia Kolbina on 26.07.17.
+//  Copyright © 2017 com.SVMaseenger. All rights reserved.
+//
+
+import UIKit
+
+let imageCashe = NSCache<AnyObject, AnyObject>()
+
+extension UIImageView {
+    
+    func loadImageUsingCasheWithUrlString(urlString: String) {
+        
+        self.image = nil
+        
+        // check cashe for image first
+        if let cashedImage = imageCashe.object(forKey: urlString as AnyObject) as? UIImage {
+            self.image = cashedImage
+            return
+        }
+        
+        //otherwise fire off a new download
+        let url = URL(string: urlString)
+        URLSession.shared.dataTask(with: url!, completionHandler: { (data, response, error) in
+            
+            // download hit an error so lets out
+            if error != nil {
+                print(error!)
+                return
+            }
+            DispatchQueue.main.async(execute: {
+                if let downloadedImage = UIImage(data: data!) {
+                imageCashe.setObject(downloadedImage, forKey: urlString as AnyObject)
+                
+                self.image = downloadedImage
+                }
+            })
+        }).resume()
+    }
+}
--- SVMaseenger/SVMaseenger/NewMessageController.swift
+++ SVMaseenger/SVMaseenger/NewMessageController.swift
@@ -55,21 +55,66 @@ class NewMessageController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-//        let use a hack for now, we actually need to dequeue our cells for memory efficiency
-//        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellId)
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as? UserCell
         let user = users[indexPath.row]
-        cell.textLabel?.text = user.name
-        cell.detailTextLabel?.text = user.email
+        cell?.textLabel?.text = user.name
+        cell?.detailTextLabel?.text = user.email
         
-        return cell
+        if let profileImageUrl = user.profileImageUrl {
+            
+            cell?.profileImageView.loadImageUsingCasheWithUrlString(urlString: profileImageUrl)
+//            let url = URL(string: profileImageUrl)
+//            URLSession.shared.dataTask(with: url!, completionHandler: { (data, response, error) in
+//                
+//                // download hit an error so lets out
+//                if error != nil {
+//                    print(error!)
+//                    return
+//                }
+//                DispatchQueue.main.async(execute: {
+//                    cell?.profileImageView.image = UIImage(data: data!)
+//                })
+//            }).resume()
+        }
+        
+        return cell!
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 72
     }
 }
 
 class UserCell: UITableViewCell {
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        textLabel?.frame = CGRect(origin: CGPoint(x: 64, y: textLabel!.frame.origin.y - 2), size: CGSize(width: textLabel!.frame.width, height: textLabel!.frame.height))
+        
+        detailTextLabel?.frame = CGRect(origin: CGPoint(x: 64, y: detailTextLabel!.frame.origin.y + 2), size: CGSize(width: detailTextLabel!.frame.width, height: detailTextLabel!.frame.height))
+    }
+    let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "male")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.layer.cornerRadius = 24
+        imageView.layer.masksToBounds = true
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+        
+        addSubview(profileImageView)
+        
+        // ios 9 constrain anchors
+        //need x, y, wight,height anchors
+        profileImageView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 8).isActive = true
+        profileImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        profileImageView.widthAnchor.constraint(equalToConstant: 48).isActive = true
+        profileImageView.heightAnchor.constraint(equalToConstant: 48).isActive = true
+        
     }
     
     required init?(coder aDecoder: NSCoder) {
